efccaa4e84e2b9879fcdd63b87da729a1eb60f4e
Kirill <seliutin.web@gmail.com>
2021-11-02 15:39:06 +0300

    end

---
+++ prechy/app.js
@@ -0,0 +1,20 @@
+$(function() {
+
+
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+
+
+
+    /* navToggle */
+
+    navToggle.on("click", function(event) {
+
+        event.preventDefault();
+
+        nav.toggleClass("show");
+
+    });
+
+});
