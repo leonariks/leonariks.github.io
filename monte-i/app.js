040c3d7df80c9fcd2cbd307deb3ae70623456d26
Kirill <89861337+leonariks@users.noreply.github.com>
2023-08-20 16:06:27 +0200

    onclick

---
+++ monte-i/app.js
@@ -0,0 +1,9 @@
+function motoc(){
+  document.getElementById('car').style.display='none';
+  document.getElementById('moto').style.display='block';
+}
+
+function cars(){
+  document.getElementById('moto').style.display='none';
+  document.getElementById('car').style.display='block';
+}
