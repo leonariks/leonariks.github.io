68211d73be4a963826a434a2bf9819d47ce66da1
Kirill <89861337+leonariks@users.noreply.github.com>
2023-08-20 17:37:36 +0200

    JS_car_change_underline

--- monte-i/app.js
+++ monte-i/app.js
@@ -1,9 +1,37 @@
+
+/** onclick Moto
+ * ====================================
+*/
 function motoc(){
   document.getElementById('car').style.display='none';
+  document.getElementById('cars').style.borderBottom='none';
+  document.getElementById('spares').style.display='none';
+  document.getElementById('spar').style.borderBottom='none';
   document.getElementById('moto').style.display='block';
+  document.getElementById('motob').style.borderBottom='2px solid #f35e49';
 }
 
+/** onclick Car
+ * ====================================
+*/
 function cars(){
   document.getElementById('moto').style.display='none';
+  document.getElementById('motob').style.borderBottom='none';
+  document.getElementById('spares').style.display='none';
+  document.getElementById('spar').style.borderBottom='none';
   document.getElementById('car').style.display='block';
+  document.getElementById('cars').style.borderBottom='2px solid #f35e49';
+}
+
+/** onclick Spares
+ * ====================================
+*/
+
+function spares(){
+  document.getElementById('moto').style.display='none';
+  document.getElementById('motob').style.borderBottom='none';
+  document.getElementById('car').style.display='none';
+  document.getElementById('cars').style.borderBottom='none';
+  document.getElementById('spares').style.display='block';
+  document.getElementById('spar').style.borderBottom='2px solid #f35e49';
 }
