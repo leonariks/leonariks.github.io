dff29b116047d579b879949d884a490585504d9a
Kirill <89861337+leonariks@users.noreply.github.com>
2023-08-24 19:18:03 +0200

    eng/rus

--- school/main/app.js
+++ school/main/app.js
@@ -6,3 +6,11 @@
   $(this).toggleClass("active");
   $("#nav").toggleClass("active");
 });
+
+$("#nav_toggles").on("click", function(event) {
+  event.preventDefault();
+
+  $(this).toggleClass("active");
+  $("#navs").toggleClass("active");
+});
+
