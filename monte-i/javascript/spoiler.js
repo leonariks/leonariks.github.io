f303a4a24cb218364c4dd321f3706c7b9aaba3ff
Kirill <89861337+leonariks@users.noreply.github.com>
2023-08-31 14:27:51 +0200

    Medicine & Advertising-4

--- monte-i/javascript/spoiler.js
+++ monte-i/javascript/spoiler.js
@@ -1,3 +1,6 @@
+/**Transport
+ * ================================
+ */
 function first(){
   document.getElementById('first').style.display='block';
   document.getElementById('second').style.display='none';
@@ -10,4 +13,24 @@ function second(){
   document.getElementById('first').style.display='none';
   document.getElementById('arrow').style.display='none';
   document.getElementById('arrow_second').style.display='block';
+}
+
+
+
+/**Medicine
+ * ================================
+ */
+
+function med__first(){
+  document.getElementById('med__first').style.display='block';
+  document.getElementById('med__second').style.display='none';
+  document.getElementById('medarrow_second').style.display='none';
+  document.getElementById('medarrow').style.display='block';
+}
+
+function med__second(){
+  document.getElementById('med__second').style.display='block';
+  document.getElementById('med__first').style.display='none';
+  document.getElementById('medarrow').style.display='none';
+  document.getElementById('medarrow_second').style.display='block';
 }
